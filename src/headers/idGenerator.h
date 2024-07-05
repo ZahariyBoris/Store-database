30f787a2527ab5e56607a87ab2b42bf86d6c7e52
ZaharBoris <zahar4ik25@gmail.com>
2024-07-05 17:23:13 +0300

    Updated function, that now makes unique product ID

--- src/headers/idGenerator.h
+++ src/headers/idGenerator.h
@@ -1,8 +1,16 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <set>
+
+std::set<int> generatedIDs;
 
 int generateID() {
     srand(time(NULL));
-    return rand() % 9000000 + 1000000;
+    int id;
+    do {
+        id = rand() % 9000000 + 1000000;
+    } while (generatedIDs.find(id) != generatedIDs.end());
+    generatedIDs.insert(id);
+    return id;
 }
